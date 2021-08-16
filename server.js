02a6d68bd2d00ef140503543dc3b78f00644c18d
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-08-16 12:59:09 -0400

    Save and Get Data functions

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require ("express");
 const application = express ();
 const path = require ("path");
+const fs = require ('fs');
 
 //Redirecting static file reqs to public file path
 application.use(express.static("public"));
+
+application.get("/api/notes",(req,res) => {
+
+});
+
+application.post("/api/notes",(req,res) => {
+
+});
+
+application.delete("/api/notes/:id",(req,res) => {
+
+});
 
 
 
@@ -18,3 +31,14 @@ application.get('/',(req,res) => {
 application.listen(3000, () => {
     console.log("server listening on port 3000");
 });
+
+function getData () {
+    const text = fs.readFileSync("./data/notes.json");
+    const json = JSON.parse(text);
+    return json;
+}
+
+function saveData (json) {
+    const text = JSON.stringify(json);
+    fs.writeFileSync("./data/notes.json",text);
+}
